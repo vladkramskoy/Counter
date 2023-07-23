043c3fc674968af53429657e6248e5b7d073a12d
Vlad Kramskoy <vkto5609@gmail.com>
2023-07-23 16:28:03 +0300

    Создал UIButton и UiLabel, подключил их к сториборду, добавил экшен (при нажатии кнопки меняется текст лейбла)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var bigRedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private var score = 0
+    
+    @IBAction func pushButton(_ sender: Any) {
+        score += 1
+        labelScore.text = "Значение счетчика: \(score)"
+        print(score)
+    }
 }
-
